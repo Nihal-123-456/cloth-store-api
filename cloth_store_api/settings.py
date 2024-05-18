bfcbf75cb39e95ee3b05d767c93785a20012f837
unknown <you@example.com>
2024-05-18 22:45:13 +0600

    third

--- cloth_store_api/settings.py
+++ cloth_store_api/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-l*0&i2pn26=w^5m(g09d2@2sy8$b0n)66%lc-vp^ina-j1xozk
 DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = []
+CSRF_TRUSTED_ORIGINS = ['https://cloth-store-api.onrender.com','https://*.127.0.0.1']
 CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
