e4b5bbeac408ab01e7540eaefec187cfa7687e62
unknown <you@example.com>
2024-08-13 18:05:11 +0600

    added port

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-
+PORT = os.environ.get('PORT', '8000')
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cloth_store_api.settings')
