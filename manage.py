cee3e8c0ea0238fb20bb53caa97a9aae89eb1318
unknown <you@example.com>
2024-08-14 11:01:23 +0600

    changed requirements

--- manage.py
+++ manage.py
@@ -14,9 +14,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    port = os.environ.get('PORT')
-    if port:
-        sys.argv += [f"0.0.0.0:{port}"]
+
     execute_from_command_line(sys.argv)
 
 
