2d702c7d0b406defef5d46a5cdfaf7316dfcae1d
Nihal-123-456 <knasn1526@example.com>
2024-10-25 21:56:23 +0600

    added dockerfile

--- cloth_store_api/settings.py
+++ cloth_store_api/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l*0&i2pn26=w^5m(g09d2@2sy8$b0n)66%lc-vp^ina-j1xozk'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
